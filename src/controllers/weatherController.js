f8bcf70a3ce4bb1117772f6dcccfdcbfad0df530
vipankaj006 <vipankaj006@gmail.com>
2023-10-01 00:43:41 +0530

    edited

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -27,6 +27,13 @@ async function saveDataToDatabase(data) {
 
 // Level 1: Get City Weather Data by Name
 async function getWeatherDataByName(cityName) {
+  const data = await getDataFromDatabase();
+  const cityData = data.find((entry)=>entry.city.toLowerCase()===cityName.toLowerCase());
+  if(cityData){
+    return cityData.weather
+  }else{
+    throw new err("City not found")
+  }
   
 }
 
